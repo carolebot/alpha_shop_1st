6a1d8c880eabe010b274f2fc6e264f218e14a0be
carolebot <carol.tang79@gmail.com>
2021-10-05 20:01:00 +0800

    feat: add stepper RWD

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 import './scss/main.scss'
 console.log('JS loaded!')
 
+const form = document.querySelector('a-form')
+const formParts = form.querySelectorAll('.part')
+const stepControl = document.getElementById('step-control')
+const steps = stepControl.querySelectorAll('.step')
+const btnControl = document.getElementById('btn-control')
+const nextBtn = btnControl.querySelector('.btn-primary')
+const prevBtn = btnControl.querySelector('.btn-outline')
+
+
+  // ; (function () {
+  //   products.forEach((product) => {
+  //     cardList.innerHTML += ``
+  //   })
+  // })()
+
+
